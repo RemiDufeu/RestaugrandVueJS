3674e21844851ec6c2bb587c93db58edc7ce2add
Ewen Hanni <48356811+ewen-hanni@users.noreply.github.com>
2021-10-05 09:27:05 +0200

    install vue material

---
+++ client/restaugrand/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+     <md-button>Default</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
