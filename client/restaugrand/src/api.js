e9f5deaf3047cfffa9434fcb4396c621100cf8a1
Rémi D <dufeu.remi@gmail.com>
2021-10-19 10:02:05 +0200

    #3 affichage des restaurants

--- client/restaugrand/src/api.js
+++ client/restaugrand/src/api.js
@@ -1,21 +1,21 @@
 export default class ApiRestau {
-    static root = "http://localhost:8080/api/restaurants/"
+    static root = "http://localhost:8080/api/restaurants"
 
-    static async getRestaurant() {
-        let dataRestau = await fetch(this.root)
+    static async getRestaurant(pageSize = 10, currentPage = 1,name = '') {
+        let dataRestau = await fetch(`${this.root}?pagesize=${pageSize}&page=${currentPage}&name=${name}`)
         let jsonRestau = await dataRestau.json()
         return jsonRestau.data
     }
 
     static async getRestaurantCount() {
-        let dataCountRestau = await fetch(`${this.root}count`)
+        let dataCountRestau = await fetch(`${this.root}/count`)
         let jsonCountRestau = await dataCountRestau.json()
         return jsonCountRestau.data
     }
 
     async supprimerRestaurant(id) {
         try {
-            let reponseJSON = await fetch(this.apiBaseURL + "/" + id, {
+            let reponseJSON = await fetch(this.root + "/" + id, {
                 method:"DELETE"
             });
             let reponseJS = await reponseJSON.json();
--- client/restaugrand/src/components/tabRestaurant.vue
+++ client/restaugrand/src/components/tabRestaurant.vue
@@ -1,10 +1,31 @@
 <template>
-    <div class="">
+    <div class="mx-6" style="width : 100%">
         <h2>Trouver votre restaurant parmis les {{count}} présents sur notre site</h2>
         <v-divider/>
         <v-autocomplete label="Cherchez le nom de votre restaurant"></v-autocomplete>
-        <button  v-on:click="getRestau">get restau</button>
-        {{data}}
+        <v-data-table
+        :headers="headers"
+        :items="dataRestau"
+        class="elevation-1"
+         hide-default-footer
+        :items-per-page="pageSize"
+        >
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            @click="editItem(item)"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon
+            small
+            @click="deleteItem(item)"
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+        </v-data-table>
     </div>
 </template>
 
@@ -16,18 +37,39 @@ import APIRestau from '../api'
     name: 'tableauRestaurant',
     data : function () {
       return {
-        data : [],
+        dataRestau : [],
         count : '',
+        headers: [
+        { text: 'Nom du restaurant', align: 'start', sortable: false, value: 'name'},
+        { text: 'Catégorie', value: 'cuisine', sortable: false },
+        { text: 'Arrondissement', value: 'borough', sortable: false },
+        { text: 'Actions', value: 'actions', sortable: false }
+      ],
+        pageSize : 11,
+        currentPage : 1,
+        name : ''
       }
     },
     components: {
     },
     mounted (){
-      APIRestau.getRestaurantCount().then(res => this.count = res)
+      this.getRestauCount()
+      this.getRestau()
     },
     methods : {
       getRestau () {
-        APIRestau.getRestaurant().then(res => this.data = res)
+        APIRestau.getRestaurant(this.pageSize, this.currentPage, this.name).then(res => this.dataRestau = res)
+      },
+      getRestauCount () {
+         APIRestau.getRestaurantCount().then(res => this.count = res)
+      },
+      deleteItem(item) {
+        //TODO
+        console.log(item)
+      },
+      editItem(item) {
+        //TODO
+        console.log(item)
       }
     }
   }
