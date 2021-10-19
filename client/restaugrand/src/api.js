6f488b226f40f27820f7ab1ff768a85f21df4f94
Ewen Hanni <48356811+ewen-hanni@users.noreply.github.com>
2021-10-19 08:46:23 +0200

    API supprimerRestaurant #4

--- client/restaugrand/src/api.js
+++ client/restaugrand/src/api.js
@@ -12,4 +12,18 @@ export default class ApiRestau {
         let jsonCountRestau = await dataCountRestau.json()
         return jsonCountRestau.data
     }
+
+    async supprimerRestaurant(id) {
+        try {
+            let reponseJSON = await fetch(this.apiBaseURL + "/" + id, {
+                method:"DELETE"
+            });
+            let reponseJS = await reponseJSON.json();
+            console.log("Restaurant supprime : " + reponseJS.msg);
+            this.getDataFromServer(); // on rafraichit l'affichage
+        } catch(err) {
+            console.log("Erreur dans le fetchs DELETE " + err.msg);
+        }
+    }
+    
 }
